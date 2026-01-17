e8aef60c01ad90532bb0ebb4a88bf3048686037a
Anishh-08 <anish.annambhotla1@gmail.com>
2026-01-17 19:27:57 +0530

    first trial prgrm from yt

--- task1code.py
+++ task1code.py
@@ -1,4 +1,14 @@
-print("L-system Fractal Architect")
-print("this is a trial commit")
+import turtle
 
-
+polygon=turtle.Turtle()
+num_poly=8
+num_sides=8
+side_length=70
+angle1=135.0
+angle=360/num_sides
+for j in range (num_poly):
+    for i in range(num_sides):
+        polygon.forward(side_length)
+        polygon.right(angle)
+    polygon.left(angle1)
+turtle.done()
