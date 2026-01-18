7e62952c3786cf627cf56f3ab15c9715faa3d8e2
Anishh-08 <anish.annambhotla1@gmail.com>
2026-01-18 14:52:15 +0530

    exploring tkinter

--- tk_turtle_test.py
+++ tk_turtle_test.py
@@ -1,1 +1,22 @@
-print("hello world")
+import tkinter as tk
+root=tk.Tk()
+root.title("My first GUI")
+root.geometry("500x500")
+
+label=tk.Label(root, text="Exploring GUI")
+label.pack(padx=10,pady=10)
+
+textbox=tk.Text(root,height=4)
+textbox.pack(padx=10,pady=10)
+
+entry=tk.Entry(root)
+entry.pack(padx=10,pady=10)
+
+button=tk.Button(root, text="Generate")
+button.pack(padx=10,pady=10)
+
+canvas=tk.Canvas(root,height=500,bg="white")
+canvas.pack(fill="x",padx=10,pady=10)
+
+root.mainloop()
+
