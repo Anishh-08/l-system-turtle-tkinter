7c9fa0f23dcf8959cde6075fae63ac9426ab7c90
Anishh-08 <anish.annambhotla1@gmail.com>
2026-01-18 15:51:40 +0530

    Introducing turtle in the canvas and customizing its properties

--- final_project.py
+++ final_project.py
@@ -7,7 +7,7 @@ root=tk.Tk()
 root.title("L-Systems")
 root.geometry("800x800")
 
-canvas=tk.Canvas(root,height=500,bg="white")
+canvas=tk.Canvas(root,height=700,bg="white")
 canvas.pack(fill="x",padx=10,pady=10)
 
 button=tk.Button(root, text="Generate")
@@ -31,6 +31,22 @@ label3.pack(padx=10,pady=10)
 entry2=tk.Entry(root)
 entry2.pack(padx=10,pady=10)
 
+screen=turtle.TurtleScreen(canvas) #Tells turtle to use that area
+t=turtle.RawTurtle(canvas) #A turtle living inside that canvas
+
+screen.bgcolor("yellow")
+#TURTLE CUSOMTIZATION
+t.shape("turtle")
+t.pensize(5)
+t.pencolor("red")
+t.speed(6)
+
+
+#POSITION TURTLE TO BOTTOM OF THE SCREEN
+t.penup()
+t.goto(740.0,-340.0)
+t.left(90)
+t.pendown()
 
 root.mainloop()
 
